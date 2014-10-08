ae9f98169e125e31537d8132fcdcfa6215de7d35
zhangyang <zyterence@gmail.com>
2014-10-08 19:13:49 +0800

    Initial Commit

---
+++ Weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weibo
+//
+//  Created by zhangyang on 14-10-8.
+//  Copyright (c) 2014年 Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
